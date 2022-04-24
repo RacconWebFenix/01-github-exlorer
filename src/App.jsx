53b137cc691c589fa3d292dd320e9ce828d896f8
Marcos Domingues <marcos.domingues@gcbinvestimentos.com>
2022-04-24 15:17:36 -0300

    Primeiro componente React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from "./components/RepositoriesList";
 import "./styles/global.scss";
 
 export function App() {
-  return <h1>Hello World - Rumo a Dominação Mundial</h1>;
+  return <RepositoryList />;
 }
---
+++ src/components/RepositoriesList.jsx
@@ -0,0 +1,29 @@
+const repositoryName = "unform2";
+
+export function RepositoryList() {
+  return (
+    <section className="repositoryList">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar Repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
