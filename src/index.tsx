47f6392762397e8f33246eacbc5a2894ab5b8efc
Marcos Domingues <marcos.domingues@gcbinvestimentos.com>
2022-04-25 08:13:05 -0300

    TypeScript no ReactJS

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import { App } from "./App";
 
 const container = document.getElementById("root");
 
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
